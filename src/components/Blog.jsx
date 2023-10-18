bcda26317f50ce57e47adb4d02947e969a2634d4
Karavang <k4r4v4ng@gmail.com>
2023-10-18 16:47:22 +0200

    little route

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -10,7 +10,7 @@ const Blog = () => {
     const fetchData = async () => {
       try {
         axios.defaults.baseURL = "https://shetagri-back.vercel.app";
-        const response = await axios.get("/");
+        const response = await axios.get("/posts");
         setData(response.data);
         setLoading(false);
       } catch (error) {
