d90fc1f8a423ba4cd8013f6dbb23074793158844
Karavang <k4r4v4ng@gmail.com>
2023-10-20 17:56:10 +0200

    test4

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import AboutMe from "./components/AboutMe";
 import Header from "./components/Header";
 import Home from "./components/Home";
@@ -10,14 +10,12 @@ function App() {
   return (
     <>
       <Header />
-      <HashRouter>
-        <Routes>
-          <Route index element={<AboutMe />} />
-          <Route path="/about" element={<Home />} />
-          <Route path="/methods" element={<Methods />} />
-          <Route path="/blog" element={<Blog />} />
-        </Routes>
-      </HashRouter>
+      <Routes>
+        <Route index element={<AboutMe />} />
+        <Route path="/about" element={<Home />} />
+        <Route path="/methods" element={<Methods />} />
+        <Route path="/blog" element={<Blog />} />
+      </Routes>
     </>
   );
 }
