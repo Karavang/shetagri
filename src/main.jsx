7b1558c2df3519935f36416a0b20f02b3bebddfa
Karavang <k4r4v4ng@gmail.com>
2023-10-29 18:11:20 +0100

    for render test

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 import "../i18n";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/shetagri">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
